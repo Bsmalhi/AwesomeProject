567e22ecb485fb82e17ead780ed14f595ffb15a2
bmalhi <bmalhi@cisco.com>
2019-05-13 18:50:12 -0700

    first commit

---
+++ src/main/java/com/awesome/AwesomeProject/AwesomeProjectApplication.java
@@ -0,0 +1,13 @@
+package com.awesome.AwesomeProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwesomeProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwesomeProjectApplication.class, args);
+	}
+
+}
